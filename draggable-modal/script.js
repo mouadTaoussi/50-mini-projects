c8a73fc753e461a89cb4e710523616d9fafc106f
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-08-16 22:41:47 +0100

    adding draggable modal

--- draggable-modal/script.js
+++ draggable-modal/script.js
@@ -14,54 +14,38 @@ const upperBar = document.querySelector('.upper-bar');
 upperBar.ondragstart = (event)=>{
 	// Disable text selection
 	document.querySelector('.content').classList.add('disable-text-selection');
-	console.log("dragStart")
-	// modal.style.top = event.clientY+ "px";
-	// modal.style.left = event.clientX+ "px";
 }
 
 upperBar.ondrag = (event)=>{
-	const offsetX = event.clientX - parseInt(modal.style.left.slice(0,3));
-	console.log("offset 1:"+offsetX)
-	console.log("offset 2:"+event.clientX)
-	modal.style.top = event.clientY+ "px";
-	modal.style.left = event.clientX+ "px";
+	// const offsetX = event.clientX - (event.clientX - parseInt(modal.style.left.slice(0,3)));
+	
+	if (event.clientX < 0) {
+		modal.style.left = "0px";
+	}
+	else if (event.clientY < 0) {
+		modal.style.top = "0px";
+	}
+	else {
+		modal.style.top = event.clientY+ "px";
+		modal.style.left = event.clientX + "px";
+		// modal.style.left = offsetX + "px";
+	}
 }
 
 upperBar.ondragend = (event)=>{
-	console.log("dragend")
+	// const offsetX = event.clientX - (event.clientX - parseInt(modal.style.left.slice(0,3)));
 	// Disable text selection
 	document.querySelector('.content').classList.remove('disable-text-selection');
-	modal.style.top = event.clientY+ "px";
-	modal.style.left = event.clientX+ "px";
+	
+	if (event.clientX < 0) {
+		modal.style.left = "0px";
+	}
+	else if (event.clientY < 0) {
+		modal.style.top = "0px";
+	}
+	else {
+		modal.style.top = event.clientY+ "px";
+		modal.style.left = event.clientX + "px";
+		// modal.style.left = offsetX + "px";
+	}
 }
-
-
-// 
-
-// upperBar.addEventListener('dblClick', () => {
-// 	console.log('dbl click ')
-// 	// Disable text selection
-// 	document.querySelector('.content').classList.add('disable-text-selection');
-// })
-
-// upperBar.addEventListener('dragStart', (event)=>{
-// 	console.log('Drag start')
-// 	modal.style.top = event.clientY+ "px";
-// 	modal.style.left = event.clientX+ "px";
-// })
-
-// upperBar.addEventListener('drag', (event)=>{
-// 	console.log('Drag ')
-// 	// Disable text selection
-// 	// document.querySelector('.content').classList.add('disable-text-selection');
-// 	modal.style.top = event.clientY+ "px";
-// 	modal.style.left = event.clientX+ "px";
-// })
-
-// upperBar.addEventListener('dragEnd', (event)=>{
-// 	console.log('Drag end')
-// 	// Disable text selection
-// 	document.querySelector('.content').classList.remove('disable-text-selection');
-// 	modal.style.top = event.clientY+ "px";
-// 	modal.style.left = event.clientX+ "px";
-// })
