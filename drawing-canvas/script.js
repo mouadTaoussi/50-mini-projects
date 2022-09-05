a7037e8eb0fc0e8c2f3e804a55319f761dbbe583
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-09-05 23:40:46 +0100

    added scroll-animation ✔

--- drawing-canvas/script.js
+++ drawing-canvas/script.js
@@ -1,25 +1,31 @@
-const canvas = document.querySelector('#canvas')
+const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+const height = canvas.height;
+const width = canvas.width;
 let clicked = false;
 let color = "black";
-let size = 20
+let size = 20;
 
 function changeColor(e){
-	color = e.path[0].value
+	color = e.path[0].value;
 }
 function changeSize(e){
-	size = e.path[0].value
+	size = e.path[0].value;
 }
-	
-	canvas.onmousedown = function (){
-		clicked = true
-	}
-	canvas.onmouseup = function (){
-		clicked = false
-	}
+function clearCanvas(){
+    ctx.clearRect(0,0,width,height);
+}
+
+
+canvas.onmousedown = function (){
+	clicked = true;
+}
+canvas.onmouseup = function (){
+	clicked = false;
+}
 canvas.onmousemove = function (e){
 	if (clicked){
-		arcs(e.offsetX, e.offsetY, size, color)
+		arcs(e.offsetX, e.offsetY, size, color);
 		// line(e.offsetX,e.offsetY)
 	}
 }
@@ -27,7 +33,7 @@ canvas.onmousemove = function (e){
 function arcs (x,y,size,color){
 	ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.fillStyle = color
+    ctx.fillStyle = color;
     ctx.fill();
 }
 // function line(x,y) {
