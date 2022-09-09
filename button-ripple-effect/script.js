fa74012577e41b152f82e129f6a8f5d8468eed1d
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-09-09 23:56:13 +0100

    adding button-ripple-effect 🚀

---
+++ button-ripple-effect/script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+
+button.onclick = (e)=>{
+	const ripple = e.path[0].children[0];
+	const rippleHeight = ripple.clientHeight / 2;
+	const rippleWidth = ripple.clientWidth / 2;
+
+	ripple.style.top = e.offsetY - rippleHeight + 'px';
+	ripple.style.left = e.offsetX - rippleWidth + 'px';
+	ripple.classList.add('scale');
+	
+	setTimeout(()=>{	
+		ripple.classList.remove('scale')
+	},500)
+}
