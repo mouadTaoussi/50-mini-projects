37adf567234a3379708caa405ff1fa87b58ed835
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-09-13 22:47:53 +0100

    adding realtime-search 🚀

---
+++ realtime-search/users.js
@@ -0,0 +1,75 @@
+const data = [{
+	name: "MouadT",
+	avatar: 'https://avatars.githubusercontent.com/u/50294614?v=4',
+	post: 'software developer',
+	degree: 'associate degree'
+},{
+	name: "Jhone doe",
+	avatar: './images/jhone_doe.jpg',
+	post: 'software enginner',
+	degree: 'Bachelor degree'
+},{
+	name: "Jane Doe",
+	avatar: './images/jane_doe.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Daniel Kumar",
+	avatar: 'https://randomuser.me/api/portraits/men/70.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Georgia Gregory",
+	avatar: 'https://randomuser.me/api/portraits/women/85.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Marta Núñez",
+	avatar: 'https://randomuser.me/api/portraits/women/50.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Lisa Dubois",
+	avatar: 'https://randomuser.me/api/portraits/women/96.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Elling Wulff",
+	avatar: 'https://randomuser.me/api/portraits/men/62.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Harry Wilson",
+	avatar: 'https://randomuser.me/api/portraits/men/72.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Leanne Fortin",
+	avatar: 'https://randomuser.me/api/portraits/women/70.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Jane Doe",
+	avatar: './images/jane_doe.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Jane Doe",
+	avatar: './images/jane_doe.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Jane Doe",
+	avatar: './images/jane_doe.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+},{
+	name: "Jane Doe",
+	avatar: './images/jane_doe.jpg',
+	post: 'back-end developer',
+	degree: 'PHD'
+}
+
+
+]
+
