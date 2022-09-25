1c15e9d21894b5d4c95be5f906b971d8cd98b1b8
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-09-25 16:36:36 +0100

    modified weather-app 👍

--- weather-app/script.js
+++ weather-app/script.js
@@ -57,37 +57,37 @@ if (window.navigator.geolocation) {
 			}
 			// 2 Set the background image based on the current weather
 			// Clear sky 01n
-			if (icon == '01n') {
+			if (icon == '01d') {
 				// console.log('clear sky')
 				background.style.backgroundImage = 'url("./images/clear-sky.jpg")';
 				return;
 			}
 			// Sunny with clouds 02n
-			if (icon == '02n') {
+			if (icon == '02d') {
 				// console.log(' Sunny with clouds')
 				background.style.backgroundImage = 'url("./images/sunny.jpg")';
 				return;
 			}
 			// Clouds 03n 04n
-			if (icon == '03n' || icon == '04n') {
-				// console.log('clouds')
+			if (icon == '03d' || icon == '04d') {
+				console.log('clouds')
 				background.style.backgroundImage = 'url("./images/cloudy.jpg")';
 				return;
 			}
 			// Rain  09n 10n 11n
-			if (icon == '09n' || icon == '10n' || icon == '11n') {
+			if (icon == '09d' || icon == '10d' || icon == '11d') {
 				// console.log('rain')
 				background.style.backgroundImage = 'url("./images/raining.jpg")';
 				return;
 			}
 			// Snow 13n
-			if (icon == '13n') {
+			if (icon == '13d') {
 				// console.log('snow')
 				background.style.backgroundImage = 'url("./images/snow-with-trees.jpg")';
 				return;
 			}
 			// Mist 50n
-			if (icon == '50n') {
+			if (icon == '50d') {
 				// console.log('mist')
 				background.style.backgroundImage = 'url("./images/cloudy-mountains.jpg")';
 				return;
