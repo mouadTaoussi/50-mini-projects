461c4ddb9fd2c658351b312c4718cc4a66fad1f4
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-10-09 18:38:27 +0100

    Added password-typing-animation 😎

--- password-typing-animation/script.js
+++ password-typing-animation/script.js
@@ -1,5 +1,7 @@
 const password = document.querySelector('.password');
 const mask = document.querySelector('.mask');
+const svg_container = document.querySelector('.svg-animation');
+
 password.onkeypress = ()=>{
 	mask.classList.remove('up')
 	mask.classList.add('down')
@@ -11,4 +13,15 @@ password.onkeydown = ()=>{
 document.onclick = ()=>{
 	mask.classList.add('up')
 	mask.classList.remove('down')
+}
+
+svg_container.onmouseover = ()=>{
+	document.querySelectorAll('#left_eye, #right_eye').forEach((eye)=>{
+		eye.style.opacity = '0'
+	})
+}
+svg_container.onmouseleave = ()=>{
+	document.querySelectorAll('#left_eye, #right_eye').forEach((eye)=>{
+		eye.style.opacity = '1'
+	})
 }
