90b33bd65f7d39fbf6773e4b358a29acbc2d5f3a
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-10-25 23:19:26 +0100

    added upload-component 😎

--- upload-component/script.js
+++ upload-component/script.js
@@ -1,12 +1,12 @@
 const input = document.querySelector('.upload input');
-const upload = document.querySelector('.upload')
+const upload = document.querySelector('.upload');
 
 input.onmouseover = ()=>{
-	upload.classList.add('active')
+	upload.classList.add('active');
 }
 input.onmouseleave = ()=>{
 	upload.classList.remove('active');
 }
 input.onchange = (e)=> {
-	upload.children[0].children[1].innerText = input.files[0].name
+	upload.children[0].children[1].innerText = input.files[0].name;
 }
