f62375fb105b963559bf1346bed87e27b2756a88
Mouad Taoussi <mouadtaoussi0@gmail.com>
2022-11-06 23:04:41 +0100

    added macos-desktop 👌

---
+++ macos-desktop/script.js
@@ -0,0 +1,10 @@
+const navigation_bar = document.querySelector('.navigation-bar');
+
+for (var i = 0; i < navigation_bar.children.length; i++) {
+	navigation_bar.children[i].onmouseover = (e)=>{
+		e.path[0].classList.add('hovered')
+	}
+	navigation_bar.children[i].onmouseleave = (e)=>{
+		e.path[0].classList.remove('hovered')
+	}
+}
