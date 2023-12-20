7126ecc4bba08a9862f763f44033b24313b1940e
mouadTaoussi <mouadtaoussi0@gmail.com>
2023-12-20 22:15:28 +0100

    remove path element

--- macos-desktop/script.js
+++ macos-desktop/script.js
@@ -2,9 +2,11 @@ const navigation_bar = document.querySelector('.navigation-bar');
 
 for (var i = 0; i < navigation_bar.children.length; i++) {
 	navigation_bar.children[i].onmouseover = (e)=>{
-		e.path[0].classList.add('hovered')
+		// e.path[0].classList.add('hovered') // path element is depercated
+		e.target.classList.add('hovered')
 	}
 	navigation_bar.children[i].onmouseleave = (e)=>{
-		e.path[0].classList.remove('hovered')
+		// e.path[0].classList.remove('hovered') // path element is depercated
+		e.target.classList.remove('hovered')
 	}
 }
