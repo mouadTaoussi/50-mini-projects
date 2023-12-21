ccba0ecea7bb819cefaa32aaeaf170fb3e0897da
mouadTaoussi <mouadtaoussi0@gmail.com>
2023-12-21 21:59:23 +0100

    remove path element

--- realtime-search/script.js
+++ realtime-search/script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.results');
 
-window.onload = ()=>{
+window.onload = loadUsers
+
+
+function loadUsers(){
 	for (var i = 0; i < data.length; i++) {
 		container.innerHTML += `
 			<div class="card">
@@ -18,11 +21,15 @@ window.onload = ()=>{
 }
 function search(e) {
 	const results = data.filter((item)=>{
-		return item.name.includes(e.path[0].value) 
+		// const input = e.path[0].value; // path element is depercated
+		const input = e.target.value
+		return item.name.includes(input)
 	})
+	console.log(results)
 	if (results.length != 0) {
+		container.innerHTML = ""
 		for (var i = 0; i < results.length; i++) {
-			container.innerHTML = `
+			container.innerHTML += `
 			<div class="card">
 				<div class="avatar">
 					<img src="${results[i].avatar}" class="avatar-image">
@@ -37,6 +44,7 @@ function search(e) {
 		}
 	}else {
 		container.innerHTML = '<div>Nothing found!</div>'
+		// loadUsers()
 	}
 
 }
