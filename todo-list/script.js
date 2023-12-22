9f220c3558edef9bd8cc1bd7f7be869fe42d35d1
mouadTaoussi <mouadtaoussi0@gmail.com>
2023-12-22 20:49:59 +0100

    remove path element

--- todo-list/script.js
+++ todo-list/script.js
@@ -17,40 +17,42 @@ window.onload = ()=>{
 }
 function addTask(e){
 	if(e.keyCode == 13) {
-		if (e.path[0].value != "") {
+		// const input = e.path[0].value // path element is depercated
+		const input = e.target.value
+		if (input != "") {
 			tasks.innerHTML += `
 				<div 
 					class="todo" 
 					onclick="done(event)"
 					oncontextmenu="remove(event)"
-				>${e.path[0].value}</div>
+				>${input}</div>
 			`
 
 			// add todo to localstorage
 			const todos_ls = JSON.parse(ls.getItem('todos'))
 			if (todos_ls == null ) {
 				ls.setItem('todos', JSON.stringify({todos: [{
-					todo: e.path[0].value, completed: false
+					todo: input, completed: false
 				}]}))
 			}else {
 				todos_ls.todos.push({
-					todo: e.path[0].value, completed: false
+					todo: input, completed: false
 				})
 				ls.setItem('todos', JSON.stringify(todos_ls))
 			}
 		
-			e.path[0].value = "";
+			e.target.value = "";
 		}
 	}
 }
 function done(e){
-	e.path[0].classList.add('completed');
+	e.target.classList.add('completed');
 	// mention todo to be completed to localstorage
 	const todos_ls = JSON.parse(ls.getItem('todos'));
 	const todos = todos_ls.todos;
 	
 	for (var i = 0; i < todos.length; i++) {
-		if (todos[i].todo == e.path[0].innerText) {
+		if (todos[i].todo == e.target.innerText) {
 			todos[i].completed = true
 		}else {
 			continue;
@@ -60,14 +62,14 @@ function done(e){
 	}
 }
 function remove(e){
-	e.path[0].remove();
+	e.target.remove();
 	// remove todo to localstorage
 	const todos_ls = JSON.parse(ls.getItem('todos'));
 	const todos = todos_ls.todos;
 	let ûpdated_todos = [];
 
 	for (var i = 0; i < todos.length; i++) {
-		if (todos[i].todo != e.path[0].innerText) {
+		if (todos[i].todo != e.target.innerText) {
 			ûpdated_todos.push(todos[i])
 		}else {
 			continue;
